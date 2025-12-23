11133f46a39ba0759b31347d6e09dc3166f112d4
8exor <dimpalkbhardwaj@gmail.com>
2025-12-23 10:17:26 +0530

    logout work changes

--- src/utils/logoutOnce.jsx
+++ src/utils/logoutOnce.jsx
@@ -1,10 +1,13 @@
-import { setAccessToken } from "./Session";
+import { setAccessToken, getAccessToken } from "./Session";
 
 export const logoutOnce = () => {
   // console.log("what up I am goonna get yoou : ", sessionStorage.getItem("LOGOUT_IN_PROGRESS"))
   if (sessionStorage.getItem("LOGOUT_IN_PROGRESS")) return;
   // console.log("is this function is running or not ::: ");
   sessionStorage.setItem("LOGOUT_IN_PROGRESS", "true");
-  setAccessToken("");
-  window.location.replace("/");
+  if (getAccessToken()) {
+    setAccessToken("");
+    window.location.replace("/");
+
+  }
 };
